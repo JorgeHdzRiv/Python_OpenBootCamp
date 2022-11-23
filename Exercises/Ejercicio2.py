2c5219dca02af96770dc76eebcec1d1c09f24c3a
JorgeHernandez <jor.hdz.riv@gmail.com>
2022-11-23 16:34:29 -0600

    Ejercicio 2

---
+++ Exercises/Ejercicio2.py
@@ -0,0 +1,12 @@
+#Instrucciones
+#Modifica la variable del anterior ejercicio en la consola de python y 
+# después muestrala por consola para ver la modificación de la variable.
+
+
+saludo = "Hola mundo!"
+
+print("El primer saludo es: ",saludo)
+
+saludo = "Hola planetas!!!!"
+
+print("El saludo modificado es: ",saludo)
