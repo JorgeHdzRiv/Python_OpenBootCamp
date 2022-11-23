7c5d26f154f909e1b166b6cda94caaf98a632bd9
JorgeHernandez <jor.hdz.riv@gmail.com>
2022-11-23 17:41:22 -0600

    Ejercicio3

---
+++ Exercises/Ejercicio3.py
@@ -0,0 +1,11 @@
+#Escribe un programa en la consola de python que pida al usuario su peso (en kg) y estatura (en metros), 
+# calcule el índice de masa corporal y lo almacene en una variable, e imprima por pantalla la frase 
+# Tu índice de masa corporal es donde es el índice de masa corporal calculado redondeado con dos decimales.
+
+peso = float(input("Introducir peso en kg: "))
+estatura = float(input("Introducir estatura en metros: "))
+
+imc = peso / estatura ** 2
+
+print("Tu indice de masa corporal es:",round(imc,2))
+
